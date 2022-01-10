b6f6ffa99e5aa3b215bf3d047cbca7b4e5eb2ffa
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2022-01-10 22:15:44 +0100

    Save Boxes into Images DONE

--- src/ImageAnnotator.py
+++ src/ImageAnnotator.py
@@ -5,7 +5,6 @@ import tkinter.messagebox
 from shapely.geometry import box as shapelyBox
 import numpy as np
 import webbrowser
-
 import cv2 as cv
 from PIL import Image, ImageTk, ImageGrab
 import json
@@ -31,6 +30,7 @@ pil_image = ""
 
 # functions
 
+
 def chargeCategories():
     categoriesList.append("face")
     categoriesList.append("mask")
@@ -46,19 +46,27 @@ def open_file():
     global pil_image
 
     if len(selectedBoxes.winfo_children()) != 0 and annotationsSaved == False:
-        MsgBox = tk.messagebox.askquestion('unsaved Annotations',
-                                           'you have unsaved annotations , do you want to save them ?', icon='warning')
-        if MsgBox == 'yes':
+        MsgBox = tk.messagebox.askquestion(
+            "unsaved Annotations",
+            "you have unsaved annotations , do you want to save them ?",
+            icon="warning",
+        )
+        if MsgBox == "yes":
             saveAnnotation()
     for child in selectedBoxes.winfo_children():
         child.destroy()
     data.clear()
 
-    filePath = tkinter.filedialog.askopenfile(mode='rb', title="Select an image",
-                                              filetypes=[("jpg file", "*.jpg"), ("png file", "*.png")])
+    filePath = tkinter.filedialog.askopenfile(
+        mode="rb",
+        title="Select an image",
+        filetypes=[("jpg file", "*.jpg"), ("png file", "*.png")],
+    )
 
     if filePath:
-        pil_image = Image.open(filePath).resize((imageWidth, imageheight), Image.ANTIALIAS)
+        pil_image = Image.open(filePath).resize(
+            (imageWidth, imageheight), Image.ANTIALIAS
+        )
         img = ImageTk.PhotoImage(pil_image)
         app.img = img
         imageArea.delete("all")
@@ -126,10 +134,20 @@ def createBoxElement(categorie, x_start, y_start, x_end, y_end):
     y_end_input.insert(tk.END, y_end)
     y_end_input.grid(row=4, column=1)
 
-    addBox = tk.Button(inputForm, text="add Box", bg="#676FA3", fg="white",
-                       command=lambda: saveBoxData(categorie_input.get(), x_start_input.get(), y_start_input.get(),
-                                                   x_end_input.get(), y_end_input.get()))
-    addBox.grid(row=5, column=0, columnspan=2, sticky='nesw')
+    addBox = tk.Button(
+        inputForm,
+        text="add Box",
+        bg="#676FA3",
+        fg="white",
+        command=lambda: saveBoxData(
+            categorie_input.get(),
+            x_start_input.get(),
+            y_start_input.get(),
+            x_end_input.get(),
+            y_end_input.get(),
+        ),
+    )
+    addBox.grid(row=5, column=0, columnspan=2, sticky="nesw")
 
     # labels
     categorie_label = tk.Label(inputForm, text="categorie")
@@ -155,13 +173,35 @@ def saveBoxData(categorie, x1, y1, x2, y2):
     # verif overlap with other boxes
 
     if verifOverlap(int(x1), int(y1), int(x2), int(y2)):
-        tkinter.messagebox.showerror("Overlap Error",
-                                     " this box Overlap with other Boxes with more than 20% \n or the box surface is less than 40 pixels ")
+        tkinter.messagebox.showerror(
+            "Overlap Error",
+            " this box Overlap with other Boxes with more than 20% \n or the box surface is less than 40 pixels ",
+        )
     else:
         # create canavs rectangle
         id = createRectangleCanvas(x1, y1, x2, y2)
         # update data file
         data[id] = {"category": categorie, "x1": x1, "y1": y1, "x2": x2, "y2": y2}
+        # save this box into a new image file for the training phase
+        imagename = os.path.basename(filePath.name)
+        imagename = os.path.splitext(imagename)[0]
+        if pil_image.mode in ("RGBA", "P"):
+            im = pil_image.convert("RGB")
+
+        im.crop((int(x1), int(y1), int(x2), int(y2))).save(
+            "imgBoxes/"
+            + imagename
+            + "-bb-"
+            + x1
+            + "x"
+            + y1
+            + "-"
+            + x2
+            + "-"
+            + y2
+            + ".jpg"
+        )
+
         # show new entry
         showSelectedBoxe(id, data.get(id))
         annotationsSaved = False
@@ -172,11 +212,19 @@ def saveBoxData(categorie, x1, y1, x2, y2):
 def showSelectedBoxe(id, boxData):
     # create one selected box
 
-    oneSelectedBox = tk.LabelFrame(selectedBoxes, text="box : " + str(id), pady=5, bg="white", borderwidth=1)
+    oneSelectedBox = tk.LabelFrame(
+        selectedBoxes, text="box : " + str(id), pady=5, bg="white", borderwidth=1
+    )
     oneSelectedBox.idTag = id
-    info = tk.Label(oneSelectedBox, text=','.join(list(boxData.values())), bg="white")
-    updateButton = tk.Button(oneSelectedBox, text="update", bg="#676FA3", fg="white", name=str(id),
-                             command=lambda: updateOneBox(id, oneSelectedBox))
+    info = tk.Label(oneSelectedBox, text=",".join(list(boxData.values())), bg="white")
+    updateButton = tk.Button(
+        oneSelectedBox,
+        text="update",
+        bg="#676FA3",
+        fg="white",
+        name=str(id),
+        command=lambda: updateOneBox(id, oneSelectedBox),
+    )
     info.grid(row=0, column=0, sticky=tk.W)
     updateButton.grid(row=0, column=1)
 
@@ -219,11 +267,15 @@ def saveAnnotation():
 
     # Opening JSON file
     try:
-        with open('annotations.json') as infile:
+        with open("annotations.json") as infile:
             imageData = json.load(infile)
         infile.close()
     except:
-        imageData = {"urlImage": {"boxId": {"category": "", "x1": "", "y1": "", "x2": "", "y2": ""}}}
+        imageData = {
+            "urlImage": {
+                "boxId": {"category": "", "x1": "", "y1": "", "x2": "", "y2": ""}
+            }
+        }
 
     # Data to be written
     if filePath:
@@ -232,21 +284,28 @@ def saveAnnotation():
             json.dump(imageData, outfile)
         annotationsSaved = True
 
-        tkinter.messagebox.showinfo("Save Annotation",
-                                    str(len(data)) + " Annotations have been successfully saved in annotations.json")
+        tkinter.messagebox.showinfo(
+            "Save Annotation",
+            str(len(data))
+            + " Annotations have been successfully saved in annotations.json",
+        )
     else:
-        tkinter.messagebox.showerror("Error Save annotation ",
-                                     "Something went wrong ! please verify that your image is uploaded")
-
+        tkinter.messagebox.showerror(
+            "Error Save annotation ",
+            "Something went wrong ! please verify that your image is uploaded",
+        )
 
 
 def importCategories():
     global categoriesList
     # Opening JSON file
-    pathFileCategories = tkinter.filedialog.askopenfile(mode='rb', title="Import Categories",
-                                                        filetypes=[("json file", "*.json")])
+    pathFileCategories = tkinter.filedialog.askopenfile(
+        mode="rb", title="Import Categories", filetypes=[("json file", "*.json")]
+    )
     if pathFileCategories:
-        f = open(pathFileCategories.name, )
+        f = open(
+            pathFileCategories.name,
+        )
         # returns JSON object as a dictionary
         categorieDictionary = json.load(f)
 
@@ -258,11 +317,15 @@ def importCategories():
         categoriesList = list(dict.fromkeys(categoriesList))
         # Closing file
         f.close()
-        tkinter.messagebox.showinfo("Import  Categories",
-                                    str(nbCategries) + " Categories have been successfully imported")
+        tkinter.messagebox.showinfo(
+            "Import  Categories",
+            str(nbCategries) + " Categories have been successfully imported",
+        )
     except:
-        tkinter.messagebox.showerror("Error Import  Categories",
-                                     "Something went wrong ! please verify that your json file in this format \n {'categories':['category1','category1','category_n']}")
+        tkinter.messagebox.showerror(
+            "Error Import  Categories",
+            "Something went wrong ! please verify that your json file in this format \n {'categories':['category1','category1','category_n']}",
+        )
 
 
 def addCategoryGui():
@@ -270,8 +333,13 @@ def addCategoryGui():
 
     newCategoryInput = tk.Entry(addCategoryForm)
     newCategoryInput.pack()
-    addCategoryBtn = tk.Button(addCategoryForm, text="add", bg="#676FA3", fg="white",
-                               command=lambda: addCategoryFn(newCategoryInput.get(), addCategoryForm))
+    addCategoryBtn = tk.Button(
+        addCategoryForm,
+        text="add",
+        bg="#676FA3",
+        fg="white",
+        command=lambda: addCategoryFn(newCategoryInput.get(), addCategoryForm),
+    )
     addCategoryBtn.pack(fill=tk.X, pady=5)
 
     addCategoryForm.pack()
@@ -283,8 +351,9 @@ def addCategoryFn(newCategory, parent):
         categoriesList.append(newCategory)
         # remove duplicate
         categoriesList = list(dict.fromkeys(categoriesList))
-        tkinter.messagebox.showinfo("add  Category",
-                                    newCategory + " have been successfully added")
+        tkinter.messagebox.showinfo(
+            "add  Category", newCategory + " have been successfully added"
+        )
 
     # destroy form
     parent.destroy()
@@ -294,16 +363,26 @@ def deleteCategoryGui():
     deleteCategoryForm = tk.LabelFrame(leftFrame, text="delete a Category :")
 
     # selected category to be deleted
-    deletedCategoryLabel = tk.Label(deleteCategoryForm, text="category to be deleted :", anchor='w')
+    deletedCategoryLabel = tk.Label(
+        deleteCategoryForm, text="category to be deleted :", anchor="w"
+    )
     deletedCategoryLabel.pack(fill=tk.X)
-    categorie_input = ttk.Combobox(deleteCategoryForm, state="readonly", values=categoriesList)
+    categorie_input = ttk.Combobox(
+        deleteCategoryForm, state="readonly", values=categoriesList
+    )
     categorie_input.current(1)
     categorie_input.pack()
 
     # get the category to be replaced
-    deleteCategoryBtn = tk.Button(deleteCategoryForm, text="delete", bg="#676FA3", fg="white",
-                                  command=lambda: replaceCategoryFn(" ", categorie_input.get(), deleteCategoryForm,
-                                                                    "deleted"))
+    deleteCategoryBtn = tk.Button(
+        deleteCategoryForm,
+        text="delete",
+        bg="#676FA3",
+        fg="white",
+        command=lambda: replaceCategoryFn(
+            " ", categorie_input.get(), deleteCategoryForm, "deleted"
+        ),
+    )
     deleteCategoryBtn.pack(fill=tk.X, pady=5)
 
     deleteCategoryForm.pack()
@@ -313,22 +392,35 @@ def replaceCategoryGui():
     replaceCategoryForm = tk.LabelFrame(leftFrame, text="replace a Category :")
 
     # selected category to be replaced
-    replacedCategoryLabel = tk.Label(replaceCategoryForm, text="category to be replaced :", anchor='w')
+    replacedCategoryLabel = tk.Label(
+        replaceCategoryForm, text="category to be replaced :", anchor="w"
+    )
     replacedCategoryLabel.pack(fill=tk.X)
-    categorie_input = ttk.Combobox(replaceCategoryForm, state="readonly", values=categoriesList)
+    categorie_input = ttk.Combobox(
+        replaceCategoryForm, state="readonly", values=categoriesList
+    )
     categorie_input.current(1)
     categorie_input.pack()
 
     # input new category
-    newCategoryLabel = tk.Label(replaceCategoryForm, text="new category", anchor='w')
+    newCategoryLabel = tk.Label(replaceCategoryForm, text="new category", anchor="w")
     newCategoryLabel.pack(fill=tk.X)
     newCategoryInput = tk.Entry(replaceCategoryForm)
     newCategoryInput.pack(fill=tk.X)
 
     # get the category to be replaced
-    replaceCategoryBtn = tk.Button(replaceCategoryForm, text="replace", bg="#676FA3", fg="white",
-                                   command=lambda: replaceCategoryFn(newCategoryInput.get(), categorie_input.get(),
-                                                                     replaceCategoryForm, "replaced by"))
+    replaceCategoryBtn = tk.Button(
+        replaceCategoryForm,
+        text="replace",
+        bg="#676FA3",
+        fg="white",
+        command=lambda: replaceCategoryFn(
+            newCategoryInput.get(),
+            categorie_input.get(),
+            replaceCategoryForm,
+            "replaced by",
+        ),
+    )
     replaceCategoryBtn.pack(fill=tk.X, pady=5)
 
     replaceCategoryForm.pack()
@@ -349,10 +441,20 @@ def replaceCategoryFn(newCategory, oldCategory, parent, operation):
                 value["category"] = newCategory
                 compteur += 1
         for child in selectedBoxes.winfo_children():
-            child.winfo_children()[0]['text'] = ','.join(list(data.get(child.idTag).values()))
+            child.winfo_children()[0]["text"] = ",".join(
+                list(data.get(child.idTag).values())
+            )
 
-        tkinter.messagebox.showinfo("replace  Category",
-                                    str(compteur) + " occurrences of " + oldCategory + " were successfully " + operation + " " + newCategory)
+        tkinter.messagebox.showinfo(
+            "replace  Category",
+            str(compteur)
+            + " occurrences of "
+            + oldCategory
+            + " were successfully "
+            + operation
+            + " "
+            + newCategory,
+        )
 
     # destroy form
     parent.destroy()
@@ -364,7 +466,12 @@ def verifOverlap(XA1, YA1, XA2, YA2):
     A = shapelyBox(XA1, YA1, XA2, YA2)
     for box in data.values():
         # create shapely box for every rectangle
-        XB1, YB1, XB2, YB2 = int(box["x1"]), int(box["y1"]), int(box["x2"]), int(box["y2"])
+        XB1, YB1, XB2, YB2 = (
+            int(box["x1"]),
+            int(box["y1"]),
+            int(box["x2"]),
+            int(box["y2"]),
+        )
         B = shapelyBox(XB1, YB1, XB2, YB2)
 
         # surface Boxe A
@@ -396,40 +503,82 @@ app.columnconfigure(2, minsize=300)
 
 # frames
 
-leftFrame = tk.LabelFrame(app, text="left", background='#EEF2FF', width=150, height=700)
-middleFrame = tk.LabelFrame(app, text="middle", background='#EEF2FF', width=900, height=700)
-rightFrame = tk.LabelFrame(app, text="right", background='#EEF2FF', width=300, height=700)
+leftFrame = tk.LabelFrame(app, text="left", background="#EEF2FF", width=150, height=700)
+middleFrame = tk.LabelFrame(
+    app, text="middle", background="#EEF2FF", width=900, height=700
+)
+rightFrame = tk.LabelFrame(
+    app, text="right", background="#EEF2FF", width=300, height=700
+)
 
 # lefFrame
 
-uploadButon = tk.Button(leftFrame, text="upload Image", fg="white", bg="#676FA3", command=open_file)
+uploadButon = tk.Button(
+    leftFrame, text="upload Image", fg="white", bg="#676FA3", command=open_file
+)
 uploadButon.pack(ipadx=200, ipady=10)
-annotationSaving = tk.Button(leftFrame, text="save annotation", fg="white", bg="#676FA3", command=saveAnnotation)
+annotationSaving = tk.Button(
+    leftFrame, text="save annotation", fg="white", bg="#676FA3", command=saveAnnotation
+)
 annotationSaving.pack(ipadx=200, ipady=10)
-importCategories = tk.Button(leftFrame, text="import categories", fg="white", bg="#676FA3", command=importCategories)
+importCategories = tk.Button(
+    leftFrame,
+    text="import categories",
+    fg="white",
+    bg="#676FA3",
+    command=importCategories,
+)
 importCategories.pack(ipadx=200, ipady=10)
-addCategory = tk.Button(leftFrame, text="add category", fg="white", bg="#676FA3", command=addCategoryGui)
+addCategory = tk.Button(
+    leftFrame, text="add category", fg="white", bg="#676FA3", command=addCategoryGui
+)
 addCategory.pack(ipadx=200, ipady=10)
-deleteCategory = tk.Button(leftFrame, text="delete category", fg="white", bg="#676FA3", command=deleteCategoryGui)
+deleteCategory = tk.Button(
+    leftFrame,
+    text="delete category",
+    fg="white",
+    bg="#676FA3",
+    command=deleteCategoryGui,
+)
 deleteCategory.pack(ipadx=200, ipady=10)
-replaceCategory = tk.Button(leftFrame, text="replace category", fg="white", bg="#676FA3", command=replaceCategoryGui)
+replaceCategory = tk.Button(
+    leftFrame,
+    text="replace category",
+    fg="white",
+    bg="#676FA3",
+    command=replaceCategoryGui,
+)
 replaceCategory.pack(ipadx=200, ipady=10)
-help = tk.Button(leftFrame, text="help", fg="white", bg="#676FA3", command=lambda: webbrowser.open_new("Documentation.pdf"))
+help = tk.Button(
+    leftFrame,
+    text="help",
+    fg="white",
+    bg="#676FA3",
+    command=lambda: webbrowser.open_new("Documentation.pdf"),
+)
 help.pack(ipadx=200, ipady=10)
 
 # middleFrame
-imageArea = tk.Canvas(middleFrame, background="white", width=imageWidth, height=imageheight)
+imageArea = tk.Canvas(
+    middleFrame, background="white", width=imageWidth, height=imageheight
+)
 imageArea.pack()
 
 # rightFrame
-boxSelection = tk.Button(rightFrame, text="box selection", fg="white", bg="#676FA3", command=selectAbox,
-                         state=tk.DISABLED)
+boxSelection = tk.Button(
+    rightFrame,
+    text="box selection",
+    fg="white",
+    bg="#676FA3",
+    command=selectAbox,
+    state=tk.DISABLED,
+)
 boxSelection.pack(ipadx=200, ipady=10)
 
 # selected elements / boxes inside a frame in the right frame
 
 
-selectedBoxes = tk.LabelFrame(rightFrame, text="Selected Boxes", background='#EEF2FF')
+selectedBoxes = tk.LabelFrame(rightFrame, text="Selected Boxes", background="#EEF2FF")
 
 selectedBoxes.pack()
 
