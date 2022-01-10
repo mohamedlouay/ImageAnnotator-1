f2faa37035cba3eec8d409f277370d352ab86aba
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2022-01-10 20:04:36 +0100

    Start of model test

---
+++ detector.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+
+model = models.Sequential(
+    filters = 32, kernel_size
+)
